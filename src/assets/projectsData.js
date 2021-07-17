5e7a0b8fd501f3110e585ff5f672218ea7fee75c
Shruti <shrutiagrawal2232@gmail.com>
2021-07-18 00:56:14 +0530

    project image added

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -3,13 +3,21 @@ export const projectsData = [
         projectName: 'Plant Leaf Identification',
         projectDescription:
             'Plant Leaf Identification is a system which is able to classify 32 different species of plants on the basis of their leaves using digital image processing techniques. The images are first preprocessed and then their shape, color and texture based features are extracted from the processed image.',
-        imageUrl: '../../assets/proj1.PNG',
+        imageUrl: 'https://unsplash.com/photos/SpCrTUG2nu8',
         videoUrl:
             '',
         projectUrl: '',
     },
     {
         projectName: 'Portfolio Website',
+        projectDescription:
+            'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.',
+        imageUrl: '',
+        videoUrl: '',
+        projectUrl: '',
+    },
+    {
+        projectName: 'Aalekh - a blogging website',
         projectDescription:
             'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.',
         imageUrl: '',
