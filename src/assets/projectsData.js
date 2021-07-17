d46e21fb3456e9e5ebf100ff81c92221c47431b3
Shruti <shrutiagrawal2232@gmail.com>
2021-07-18 01:31:23 +0530

    project image added

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -3,9 +3,8 @@ export const projectsData = [
         projectName: 'Plant Leaf Identification',
         projectDescription:
             'Plant Leaf Identification is a system which is able to classify 32 different species of plants on the basis of their leaves using digital image processing techniques. The images are first preprocessed and then their shape, color and texture based features are extracted from the processed image.',
-        imageUrl: 'https://images.unsplash.com/photo-1526073733167-1b6d55175336?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
-        videoUrl:
-            '',
+        imageUrl: 'Plant Leaf Identification.jpg',
+        videoUrl: '',
         projectUrl: '',
     },
     {
