2c951610f59af2a2a6c33ef741305b67ad6c8203
Shruti <shrutiagrawal2232@gmail.com>
2021-07-18 01:47:10 +0530

    project image added

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -4,23 +4,23 @@ export const projectsData = [
         projectDescription:
             'Plant Leaf Identification is a system which is able to classify 32 different species of plants on the basis of their leaves using digital image processing techniques. The images are first preprocessed and then their shape, color and texture based features are extracted from the processed image.',
         imageUrl: 'Plant Leaf Identification.jpg',
-        videoUrl: '',
-        projectUrl: '',
+        videoUrl: 'https://github.com/ShrutiAgrawal2232/Plant-Leaf-Identification',
+        projectUrl: 'https://github.com/ShrutiAgrawal2232/Plant-Leaf-Identification',
     },
     {
         projectName: 'Portfolio Website',
         projectDescription:
             'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.',
-        imageUrl: '',
-        videoUrl: '',
-        projectUrl: '',
+        imageUrl: 'PortfolioBanner.jpg',
+        videoUrl: 'https://shrutiagrawal2232.github.io/#/',
+        projectUrl: 'https://shrutiagrawal2232.github.io/#/',
     },
     {
         projectName: 'Aalekh - a blogging website',
         projectDescription:
-            'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.',
+            'Your customized blogging world.',
         imageUrl: 'Banner.png',
-        videoUrl: '',
-        projectUrl: '',
+        videoUrl: 'https://github.com/ShrutiAgrawal2232/Aalekh-a-blogging-website',
+        projectUrl: 'https://github.com/ShrutiAgrawal2232/Aalekh-a-blogging-website',
     },
 ];
